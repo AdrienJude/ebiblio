9d2c3e095767b02c7a1e34a6cf48a64be67e92a3
AdrienJude <jrayaisse7@gmail.com>
2025-03-24 14:41:24 +0000

    premmier commit

---
+++ src/gesbibliothèque/GesBibliothèque.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gesbibliothèque;
+
+/**
+ *
+ * @author Adrien
+ */
+public class GesBibliothèque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formmain fm = new formmain();
+        fm.setVisible(true);
+        fm.setResizable(false);
+     
+    }
+    
+}
